88f564b67708c1af69e30667039b48276003d3b7
Chromadesk <yisraeleliyah@gmail.com>
2022-10-08 21:49:22 -0400

    Task 1 & 2 Complete

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 -- Part 1: Test it with SQL
-
+-- id = int PRIMARY_KEY, employer = varchar, name = varchar, skills = varchar
 -- Part 2: Test it with SQL
-
+SELECT name FROM techjobs.employer
+WHERE employer.location = "St. Louis City";
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,36 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(max = 60)
+    private String location;
 
+    //TODO
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Employer() {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
